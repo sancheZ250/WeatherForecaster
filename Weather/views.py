f6d75acef2704dcf4b941e752664b294b2306de8
sancheZ250 <sancheZ2502@yandex.ru>
2023-10-01 22:01:33 +0700

    Added more weather information fields. The template has been changed, for a more human appearance, coordinates are now obtained by one button.

--- Weather/views.py
+++ Weather/views.py
@@ -3,23 +3,31 @@ import requests
 
 def index(request):
     if request.method == 'POST':
-        api_id = '8c3431052deac27e1e526388cfd1769d'
+        api_key = '8c3431052deac27e1e526388cfd1769d'
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_id}&units='standart'"
+        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
 
         response = requests.get(api_url)
 
         if response.status_code == 200:
             weather_data = response.json()
-            temperature = round(weather_data['main']['temp']-272.1)
+            name = weather_data['name']
+            temperature = weather_data['main']['temp']
+            feels_like = weather_data['main']['feels_like']
             weather_condition = weather_data['weather'][0]['description']
             wind_speed = weather_data['wind']['speed']
+            humidity = weather_data['main']['humidity']
+            pressure = weather_data['main']['pressure']
 
             context = {
                 'temperature': temperature,
                 'weather_condition': weather_condition,
-                'wind_speed': wind_speed
+                'wind_speed': wind_speed,
+                'humidity': humidity,
+                'pressure': pressure,
+                'feels_like': feels_like,
+                'name': name,
             }
 
             return render(request, 'Weather/index.html', context=context)
